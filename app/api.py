192b2119b828b78f2d2d1140ca6d15f93f21485a
Akosua <akosuaasante@gmail.com>
2021-01-08 22:09:14 -0500

    basic team and player endpoints

--- app/api.py
+++ app/api.py
@@ -4,12 +4,33 @@ import requests
 
 app = Flask(__name__)
 app.config.from_envvar('APP_CONFIG_PATH')
+apikey_token = app.config['SPORTS_DATA_IO']
+headers = { "Ocp-Apim-Subscription-Key": apikey_token }
 
 
 @app.route("/")
 def hello():
-    return app.config['MY_SPORTSFEED_API_KEY']
+    return "hi"
 
+@app.route("/teams")
+def all_active_teams():
+    url = 'https://api.sportsdata.io/v3/nba/scores/json/teams'
+    response = requests.get(url=url, headers=headers)
+    print('Response HTTP Status Code: {status_code}'.format(
+            status_code=response.status_code))
+    print('Response HTTP Response Body: {content}'.format(
+            content=response.content))
+    return response.content
+
+@app.route("/players")
+def all_active_players():
+    url = 'https://api.sportsdata.io/v3/nba/scores/json/Players'
+    response = requests.get(url=url, headers=headers)
+    print('Response HTTP Status Code: {status_code}'.format(
+        status_code=response.status_code))
+    print('Response HTTP Response Body: {content}'.format(
+            content=response.content))
+    return response.content
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
