254c0f033f313ecba2dabfc18382ce9b3cd90b8f
Akosua <akosuaasante@gmail.com>
2021-01-20 15:32:00 -0500

    initial db things

--- app/api.py
+++ app/api.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import base64
-import requests
+from sportsfeed import SportsdataAPI
+import psycopg2
+
 
 app = Flask(__name__)
 app.config.from_envvar('APP_CONFIG_PATH')
@@ -12,25 +14,17 @@ headers = { "Ocp-Apim-Subscription-Key": apikey_token }
 def hello():
     return "hi"
 
-@app.route("/teams")
-def all_active_teams():
-    url = 'https://api.sportsdata.io/v3/nba/scores/json/teams'
-    response = requests.get(url=url, headers=headers)
-    print('Response HTTP Status Code: {status_code}'.format(
-            status_code=response.status_code))
-    print('Response HTTP Response Body: {content}'.format(
-            content=response.content))
-    return response.content
-
-@app.route("/players")
-def all_active_players():
-    url = 'https://api.sportsdata.io/v3/nba/scores/json/Players'
-    response = requests.get(url=url, headers=headers)
-    print('Response HTTP Status Code: {status_code}'.format(
-        status_code=response.status_code))
-    print('Response HTTP Response Body: {content}'.format(
-            content=response.content))
-    return response.content
+@app.route("/teams", methods=['POST'])
+def insert_teams():
+    sportsdata_api = SportsdataAPI(apikey_token)
+    conn = psycopg2.connect(
+        host="localhost",
+        database="analytics",
+        user="analytics",
+        password="analytics"
+    )
+    
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
