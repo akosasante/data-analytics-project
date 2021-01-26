30a183ab0d27d88aab5bb09c32fc7ddcdd95dc76
Akosua <akosasante@users.noreply.github.com>
2021-01-26 12:09:04 -0500

    Ingest data from Sportsdata API and insert into dockerized Postgres db (#2)
    * ingest base NBA data command
    
    * ingest season data from sportsdata io
    
    Co-authored-by: Akosua Asante <akosuaasante@gmail.com>

--- app/fetch_data.py
+++ app/fetch_data.py
@@ -1,34 +1,38 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from app import db, app
 import click
+import models
+from sportsdata import SportsdataAPI
 
-app = Flask(__name__)
-app.config.from_envvar('APP_CONFIG_PATH')
-db = SQLAlchemy(app)
-# apikey_token = app.config['SPORTS_DATA_IO_API_KEY']
-# headers = {"Ocp-Apim-Subscription-Key": apikey_token}
+sportsdata_api_key = app.config['SPORTS_DATA_IO_API_KEY']
+
 
 @click.group()
 def cli():
     pass
 
+
 @cli.command()
 def init_db():
-    click.echo('init_db')
-    meta = db.metadata
+    click.echo('initializing database')
+    click.echo('dropping tables')
     db.drop_all()
-    # for table in reversed(meta.sorted_tables):
-    #     print ('Clear table %s' % table)
-    #     table.delete()
-    #     db.session.commit()
+    click.echo('recreating tables')
     db.create_all()
 
 
 @cli.command()
 def init_data():
     click.echo('ingest base data')
-    click.echo('ingest nba players from sportsdata.io')
-    # call your ingestion methods.
+    click.echo('ingest nba data from sportsdata.io')
+    ingest_sportsdata_data()
+
+
+@cli.command()
+@click.option('--year', '-y', default='', help='Which year\'s stats do you want to ingest?')
+def get_year_data(year):
+    click.echo('ingest season data')
+    click.echo('ingest nba data from sportsdata.io')
+    ingest_sportsdata_data_for_year(year)
 
 
 @cli.command()
@@ -37,6 +41,14 @@ def update_entity(entity):
     for e in entity:
         click.echo(f"updating {e}")
         # call update methods...
+
+
+def ingest_sportsdata_data():
+    SportsdataAPI(sportsdata_api_key).ingest_data(click, db, models)
+
+
+def ingest_sportsdata_data_for_year(year):
+    SportsdataAPI(sportsdata_api_key).ingest_season_data(year, click, db, models)
 
 
 if __name__ == '__main__':
--- app/models.py
+++ app/models.py
@@ -1,19 +1,19 @@
-from app import api
-from sqlalchemy.dialects.postgresql import JSONB
+from app import db
+from sqlalchemy.dialects.postgresql import JSONB, BIGINT
 from datetime import datetime
 
-db = api.db
 
-class SportsDataIO_NBAPLayer(db.Model):
+class SportsDataIO_NBAPlayer(db.Model):
     __tablename__ = 'sports_data_io_nba_player'
-    id = db.Column(db.BigInteger, primary_key=True)
+    id = db.Column(BIGINT, primary_key=True)
     raw_json = db.Column(JSONB)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
+
 class NBAPlayer(db.Model):
     __tablename__ = 'nba_player'
-    player_id = db.Column(db.bigInteger, primary_key=True)
-    team_id = db.Column(db.BigInteger)
+    player_id = db.Column(BIGINT, primary_key=True)
+    team_id = db.Column(BIGINT)
     jersey_number = db.Column(db.Integer)
     position = db.Column(db.String)
     first_name = db.Column(db.String)
@@ -21,3 +21,57 @@ class NBAPlayer(db.Model):
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     is_valid_to = db.Column(db.DateTime, nullable=False)
     is_latest = True
+
+
+class SportsDataIO_NBATeam(db.Model):
+    __tablename__ = 'sports_data_io_nba_team'
+    id = db.Column(BIGINT, primary_key=True)
+    raw_json = db.Column(JSONB)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+class SportsDataIO_NBAStadium(db.Model):
+    __tablename__ = 'sports_data_io_nba_stadium'
+    id = db.Column(BIGINT, primary_key=True)
+    raw_json = db.Column(JSONB)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+class SportsDataIO_NBASeasonStandings(db.Model):
+    __tablename__ = 'sports_data_io_nba_season_standings'
+    id = db.Column(BIGINT, primary_key=True)
+    year = db.Column(db.Integer)
+    raw_json = db.Column(JSONB)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+class SportsDataIO_NBASeasonAllStars(db.Model):
+    __tablename__ = 'sports_data_io_nba_season_all_stars'
+    id = db.Column(BIGINT, primary_key=True)
+    year = db.Column(db.Integer)
+    raw_json = db.Column(JSONB)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+class SportsDataIO_NBASeasonGameStats(db.Model):
+    __tablename__ = 'sports_data_io_nba_season_game_stats'
+    id = db.Column(BIGINT, primary_key=True)
+    year = db.Column(db.Integer)
+    raw_json = db.Column(JSONB)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+class SportsDataIO_NBASeasonTeamStats(db.Model):
+    __tablename__ = 'sports_data_io_nba_season_team_stats'
+    id = db.Column(BIGINT, primary_key=True)
+    year = db.Column(db.Integer)
+    raw_json = db.Column(JSONB)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+class SportsDataIO_NBASeasonPlayerStats(db.Model):
+    __tablename__ = 'sports_data_io_nba_season_player_stats'
+    id = db.Column(BIGINT, primary_key=True)
+    year = db.Column(db.Integer)
+    raw_json = db.Column(JSONB)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
