59d576bebf05135accac59d836c9658243e1f9d4
Akosua Asante <akosuaasante@gmail.com>
2021-01-26 10:29:23 -0500

    group the cli commands

--- app/fetch_data.py
+++ app/fetch_data.py
@@ -3,13 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 import click
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
 app.config.from_envvar('APP_CONFIG_PATH')
-apikey_token = app.config['SPORTS_DATA_IO']
-headers = {"Ocp-Apim-Subscription-Key": apikey_token}
+db = SQLAlchemy(app)
+# apikey_token = app.config['SPORTS_DATA_IO_API_KEY']
+# headers = {"Ocp-Apim-Subscription-Key": apikey_token}
 
+@click.group()
+def cli():
+    pass
 
-@click.command()
+@cli.command()
 def init_db():
     click.echo('init_db')
     meta = db.metadata
@@ -21,16 +24,20 @@ def init_db():
     db.create_all()
 
 
-@click.command()
+@cli.command()
 def init_data():
     click.echo('ingest base data')
     click.echo('ingest nba players from sportsdata.io')
     # call your ingestion methods.
 
 
-@click.command()
+@cli.command()
 @click.option('--entity', '-e', default='', help='Which entity?')
 def update_entity(entity):
     for e in entity:
         click.echo(f"updating {e}")
         # call update methods...
+
+
+if __name__ == '__main__':
+    cli()
