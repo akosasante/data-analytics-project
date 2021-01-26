3bd029a851321c3b7754a9ced9e503d6e03f3dbe
Akosua Asante <akosuaasante@gmail.com>
2021-01-26 11:36:09 -0500

    ingest base NBA data command

--- app/fetch_data.py
+++ app/fetch_data.py
@@ -1,34 +1,30 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from app import db, app
 import click
+import models
+from sportsdata import SportsdataAPI
 
-app = Flask(__name__)
-app.config.from_envvar('APP_CONFIG_PATH')
-db = SQLAlchemy(app)
-# apikey_token = app.config['SPORTS_DATA_IO_API_KEY']
-# headers = {"Ocp-Apim-Subscription-Key": apikey_token}
+sportsdata_api_key = app.config['SPORTS_DATA_IO_API_KEY']
+
 
 @click.group()
 def cli():
     pass
 
+
 @cli.command()
 def init_db():
-    click.echo('init_db')
-    meta = db.metadata
+    click.echo('initializing database')
+    click.echo('dropping tables')
     db.drop_all()
-    # for table in reversed(meta.sorted_tables):
-    #     print ('Clear table %s' % table)
-    #     table.delete()
-    #     db.session.commit()
+    click.echo('recreating tables')
     db.create_all()
 
 
 @cli.command()
 def init_data():
     click.echo('ingest base data')
-    click.echo('ingest nba players from sportsdata.io')
-    # call your ingestion methods.
+    click.echo('ingest nba data from sportsdata.io')
+    ingest_sportsdata_data()
 
 
 @cli.command()
@@ -37,6 +33,10 @@ def update_entity(entity):
     for e in entity:
         click.echo(f"updating {e}")
         # call update methods...
+
+
+def ingest_sportsdata_data():
+    SportsdataAPI(sportsdata_api_key).ingest_data(db, models)
 
 
 if __name__ == '__main__':
--- app/sportsdata.py
+++ app/sportsdata.py
@@ -1,9 +1,10 @@
 import requests
+import json
 
 
 class SportsdataAPI:
     """Pull data from https://sportsdata.io api"""
-    
+
     def __init__(self, api_key):
         self.api_key = api_key
 
@@ -11,7 +12,7 @@ class SportsdataAPI:
         headers = { "Ocp-Apim-Subscription-Key": self.api_key }
         response = requests.get(url=url, headers=headers)
         if response.status_code == 200:
-            return response.content
+            return json.loads(response.content)
         else:
             raise Exception("ERROR")
 
@@ -20,7 +21,10 @@ class SportsdataAPI:
 
     def get_all_players(self):
         return self.request("https://api.sportsdata.io/v3/nba/scores/json/Players")
-    
+
+    def get_all_stadiums(self):
+        return self.request("https://api.sportsdata.io/v3/nba/scores/json/Stadiums")
+
     def get_season_standings_for_year(self, year):
         return self.request("https://api.sportsdata.io/v3/nba/scores/json/Standings/{season}".format(season=year))
 
@@ -32,3 +36,20 @@ class SportsdataAPI:
 
     def team_stats_allowed_for_season(self, year):
         return self.request("https://api.sportsdata.io/v3/nba/stats/json/TeamStatsAllowedByPosition/{season}".format(season=year))
+
+    def ingest_data(self, db, models):
+        # Players
+        all_players = models.SportsDataIO_NBAPlayer(raw_json=self.get_all_players())
+        db.session.add(all_players)
+        db.session.commit()
+
+        # Teams
+        all_teams = models.SportsDataIO_NBATeam(raw_json=self.get_all_teams())
+        db.session.add(all_teams)
+        db.session.commit()
+
+        # Stadiums
+        all_stadiums = models.SportsDataIO_NBAStadium(raw_json=self.get_all_stadiums())
+        db.session.add(all_stadiums)
+        db.session.commit()
+
